c62d7228efab164084a8b1c904fa50a94bb3ba7d
Monica Verdi <monica.verdi@gmail.com>
2020-11-16 21:16:26 +0100

    create Authors and Books model

---
+++ 00-library-app/models/Author.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const authorSchema = new Schema(
+    {
+        name: String,
+        lastName: String,
+        nationality: String,
+        birthday: Date,
+        pictureUrl: String,
+    }
+);
+
+const Author = mondoose.model("Author", authorSchema);
+
+module.exports = Author;
---
+++ 00-library-app/models/Book.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const bookSchema = new Schema(
+    {
+        title: String,
+        description: String,
+        rating: Number,
+        authors: [{ type: Schema.Types.ObjectId, ref: "Author" }],
+    },
+    {
+        title: String,
+        description: String,
+        rating: Number,
+        authors:[{type:Schema.Types.ObjectId, ref: "Author"}],
+    },
+    {
+        rinestamps:{
+            createdAt:"created_at",
+            updatedAt: "updated_at",
+        },
+    }
+);
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
